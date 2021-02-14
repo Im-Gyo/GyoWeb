fe8fc9699688ecc3bad65d07433b72a098b7576e
Gyo <jywlsh3@naver.com>
2021-02-15 00:37:49 +0900

    메인페이지 로딩 테스트

--- src/main/java/com/myapp/web/springboot/web/IndexController.java
+++ src/main/java/com/myapp/web/springboot/web/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "index"; // 머스테치 스타터로 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
     }
 }
