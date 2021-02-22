ecaca2ed529ddea0b294dd809209ec2a981dd207
Gyo <jywlsh3@naver.com>
2021-02-22 18:30:40 +0900

    PostsService에 findAllDesc 메소드 추가

--- src/main/java/com/myapp/web/springboot/service/PostsService.java
+++ src/main/java/com/myapp/web/springboot/service/PostsService.java
@@ -10,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
@@ -36,4 +39,12 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // (readOnly = true) : 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회 속도 개선(조회만 가능)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListReponseDto::new) // .map(posts -> new PostsListResponseDto(posts))와 동일
+                .collect(Collectors.toList());
+    }
+
 }
