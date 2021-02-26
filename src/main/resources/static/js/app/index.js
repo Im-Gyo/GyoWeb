8724a4a5935e5fffa0b3543949e2557dc10b0c0f
Gyo <jywlsh3@naver.com>
2021-02-26 23:17:04 +0900

    index.js에 수정, 삭제 기능 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -5,7 +5,7 @@ var main = {
             _this.save();
         });
 
-        $('#btn-update').on('click', function () {
+        $('#btn-update').on('click', function () { // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록
             _this.update();
         });
 
@@ -29,6 +29,42 @@ var main = {
         }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = '/';  // 글 등록이 성공하면 메인페이지로 이동
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT', // HTTP Method중 PUT메소드를 선택(이미 Api컨트롤러에서 @PutMapping을 사용했기 때문에 PUT을 사용해야함 (Rest규약에 맞게 설정된 것임)
+            url: '/api/v1/posts/'+id, // 어느 게시글을 수정할 지 URL Path로 구분하기 위해 Path에 id를 추가함.
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
