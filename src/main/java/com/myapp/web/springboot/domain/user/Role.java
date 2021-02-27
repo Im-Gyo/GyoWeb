f4add5f230566250beb2beb202351820c7a99873
jywls <jywlsh3@naver.com>
2021-02-27 19:41:02 +0900

    사용자 권한관리 Enum생성

---
+++ src/main/java/com/myapp/web/springboot/domain/user/Role.java
@@ -0,0 +1,16 @@
+//사용자의 권한을 관리할 Enum클래스 생성
+package com.myapp.web.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum  Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
