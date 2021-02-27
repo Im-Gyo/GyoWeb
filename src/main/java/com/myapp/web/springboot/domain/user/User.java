04d075e3f1aa412bc70624239ffe32975c37d2b1
jywls <jywlsh3@naver.com>
2021-02-27 19:38:28 +0900

    사용자 정보 담당 도메인 생성

---
+++ src/main/java/com/myapp/web/springboot/domain/user/User.java
@@ -0,0 +1,52 @@
+//사용자 정보를 담당할 도메인
+package com.myapp.web.springboot.domain.user;
+
+import com.myapp.web.springboot.domain.posts.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) // JPA로 데이터베이스로 저장될 때 Enum값을 어떤 형태로 저장할 지 결정(기본적으로는 int로된 숫자가 저장), 숫자로 저장되면 데이터베이스로 확인할 때 그 값이 무슨 코드를 의미하는지 알 수 없음(그래서 문자열로 저장될 수 있도로 선언했음)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public user(String name, String email, String picture, Role role){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+}
