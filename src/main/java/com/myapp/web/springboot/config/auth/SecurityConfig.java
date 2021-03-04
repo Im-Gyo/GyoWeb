2f41fd9e1e0acd6a2154cd612e3e72032d2564cd
Gyo <jywlsh3@naver.com>
2021-03-05 02:12:36 +0900

    구글 로그인 연동 완료

--- src/main/java/com/myapp/web/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/myapp/web/springboot/config/auth/SecurityConfig.java
@@ -5,14 +5,15 @@ import com.myapp.web.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity // Spring Security 설정들을 활성화
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-//    @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
--- src/main/java/com/myapp/web/springboot/web/IndexController.java
+++ src/main/java/com/myapp/web/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.myapp.web.springboot.web;
 
+import com.myapp.web.springboot.config.auth.dto.SessionUser;
 import com.myapp.web.springboot.service.PostsService;
 import com.myapp.web.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){ // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장, postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser)httpSession.getAttribute("user"); // CustomOAuth2Service에서 로그인 성공시 세션에 SessionUser를 저장, 즉,로그인 성공시httpSession.getAttribute("user") 에서 값을 가져올 수 있음
+
+        if(user != null){ // 세션에 저장된 값이 있을 때만 model에 userName으로 등록, 세션에 저장된 값이 없으면 로그인 버튼이 보이게 됨
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     } // 머스테치 스타터로 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
 
